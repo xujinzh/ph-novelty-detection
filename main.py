fea68156a7800753c9d948d27c02b78ed87bdff9
jinzhongxu <jinzhongxu@csu.ac.cn>
2020-11-26 09:43:47 +0800

    narrow query

--- main.py
+++ main.py
@@ -23,21 +23,20 @@ def main(cluster):
         file_name = os.path.split(data_path)[1]
         print("\t正在处理数据集 {} ...".format(file_name.split('-')[0]))
         if cluster == 'spectral' or cluster == 'hierarchical' or cluster == 'kmeans':
-            for n_cluster in range(8, 100):
+            for n_cluster in range(10, 35):
                 run.just_do_it(path=data_path, cluster=cluster, n_cluster=n_cluster)
         elif cluster == 'hierarchical':
-            for n_cluster in range(8, 100):
+            for n_cluster in range(10, 35):
                 for linkage in ['ward', 'complete', 'average', 'single']:
                     run.just_do_it(path=data_path, cluster=cluster, linkage=linkage)
         elif cluster == 'dbscan' or cluster == 'optics':
-            for eps in range(5, 15):
+            for eps in range(5, 10):
                 for min_samples in range(3, 9):
                     run.just_do_it(path=data_path, cluster=cluster, eps=eps, min_samples=min_samples)
         elif cluster == 'birch':
-            #  'n_cluster', 'branching_factor', 'threshold'
-            for n_cluster in range(8, 100):
-                for branching_factor in range(5, 15):
-                    for cluster_threshold in np.arange(0.5, 1.0, 0.1):
+            for n_cluster in range(10, 35):
+                for branching_factor in range(5, 10):
+                    for cluster_threshold in np.arange(0.5, 0.9, 0.1):
                         run.just_do_it(path=data_path, cluster=cluster, n_cluster=n_cluster,
                                        branching_factor=branching_factor, cluster_threshold=cluster_threshold)
         print("\t完成数据集 {} 的处理工作 ^_^".format(file_name.split('-')[0]))
