d9e18e18fdb286c6711d6023c4f9fbad8e80163c
jinzhongxu <jinzhongxu@csu.ac.cn>
2020-11-26 17:32:02 +0800

    update to multiprocessing

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ from tda import run
 import numpy as np
 import os
 from tda.timestamps import display_time
+from multiprocessing import Pool
+import warnings
+
+warnings.filterwarnings("ignore")
 
 
-@display_time
 def main(cluster):
     # 读取数据
     data_paths = ['./data/breast-cancer-unsupervised-ad.csv', './data/letter-unsupervised-ad.csv',
@@ -21,31 +24,34 @@ def main(cluster):
 
     for data_path in data_paths:
         file_name = os.path.split(data_path)[1]
-        print("\t正在处理数据集 {} ...".format(file_name.split('-')[0]))
-        if cluster == 'spectral' or cluster == 'hierarchical' or cluster == 'kmeans':
-            for n_cluster in range(10, 35):
+        print("\t正在使用聚类算法 {0} 处理数据集 {1} ...".format(cluster, file_name.split('-')[0]))
+        if cluster == 'spectral' or cluster == 'kmeans':
+            for n_cluster in range(10, 38):
                 run.just_do_it(path=data_path, cluster=cluster, n_cluster=n_cluster)
         elif cluster == 'hierarchical':
-            for n_cluster in range(10, 35):
+            for n_cluster in range(10, 38):
                 for linkage in ['ward', 'complete', 'average', 'single']:
-                    run.just_do_it(path=data_path, cluster=cluster, linkage=linkage)
+                    run.just_do_it(path=data_path, cluster=cluster, linkage=linkage, n_cluster=n_cluster)
         elif cluster == 'dbscan' or cluster == 'optics':
-            for eps in range(5, 10):
+            for eps in range(5, 20):
                 for min_samples in range(3, 9):
                     run.just_do_it(path=data_path, cluster=cluster, eps=eps, min_samples=min_samples)
         elif cluster == 'birch':
-            for n_cluster in range(10, 35):
-                for branching_factor in range(5, 10):
+            for n_cluster in range(10, 38):
+                for branching_factor in range(5, 50):
                     for cluster_threshold in np.arange(0.5, 0.9, 0.1):
                         run.just_do_it(path=data_path, cluster=cluster, n_cluster=n_cluster,
                                        branching_factor=branching_factor, cluster_threshold=cluster_threshold)
-        print("\t完成数据集 {} 的处理工作 ^_^".format(file_name.split('-')[0]))
+        print("\t聚类算法 {0} 完成数据集 {1} 的处理工作 ^_^".format(cluster, file_name.split('-')[0]))
 
 
 if __name__ == '__main__':
     clusters = ['spectral', 'hierarchical', 'kmeans', 'dbscan', 'optics', 'birch']
-    for cluster in clusters:
-        print("使用聚类方法 {0} 进行处理 ...".format(cluster))
-        main(cluster=cluster)
-        print("\t聚类方法 {0} 处理结束 ^_^".format(cluster))
+    # for cluster in clusters:
+    #     print("使用聚类方法 {0} 进行处理 ...".format(cluster))
+    #     main(cluster=cluster)
+    #     print("\t聚类方法 {0} 处理结束 ^_^".format(cluster))
+    # print("整个任务完成！")
+    with Pool(6) as p:
+        p.map(main, clusters)
     print("整个任务完成！")
